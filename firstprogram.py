656189a301faef059ce8ee8f783b466cc8064d51
MrVick21 <vikrantvijayloyare@gmail.com>
2024-06-28 21:05:24 +0530

    Sexy changes have been added

---
+++ firstprogram.py
@@ -0,0 +1,26 @@
+class Divison_B:
+
+    def __init__(object,name,address):#parameterix
+        object.name=name
+        object.city=address
+        print(object.name,end=" ")
+        print(object.city)
+        
+
+class DivsionC:
+   def __init__(a):
+       print(a)
+
+
+
+
+
+    
+s1=Divison_B("Ameya","Kothrud")
+s2=Divison_B("Shreya","narayanpeth")#paramterized constructor
+s3=Divison_B("Ojas","karverd")
+
+a1=DivsionC()
+a2=DivsionC()
+a3=DivsionC()
+a4=DivsionC()
