1592956382035e76c04dabcf74287bfb1c9f99da
MrVick21 <vikrantvijayloyare@gmail.com>
2024-07-02 20:21:17 +0530

    chzznged

--- firstprogram.py
+++ firstprogram.py
@@ -1,14 +1,12 @@
 class divb:
-    school="symbiosis" #class attribute
-    def __init__(obj,name):
-        obj.name=name
-    def hello(self):
-        print("Hello")
-   
-s1=divb("Shreya") #$object create
-print(s1.name,s1.school)
-s2=divb("ojas")
-print(s2.name,s2.school)
-s3=divb("aryan")
-print(s3.name,s3.school)
+    name="anonymous" #class attribute
+    school="Symbiosis"
+    def __init__(self,name,marks):
+        self.name=name #  #object attribute> class attribute   
+        self.marks=marks
+        print("Adding in databases....")
+
+s1=divb("shreya",97)
+print(s1.name)
+
 
